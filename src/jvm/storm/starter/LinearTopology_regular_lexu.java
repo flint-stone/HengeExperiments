c6ec4a65888f3c3a259575adc608a52e7c3bf048
flint-stone <sharonxu65@gmail.com>
2017-01-09 22:35:19 +0800

    m

--- src/jvm/storm/starter/LinearTopology_regular_lexu.java
+++ src/jvm/storm/starter/LinearTopology_regular_lexu.java
@@ -11,24 +11,24 @@ public class LinearTopology_regular_lexu {
 	public static void main(String[] args) throws Exception {
 
 		int numBolt = 3;
-		int paralellism = 10;
+		int paralellism = 5;
 
 		TopologyBuilder builder = new TopologyBuilder();
 
-		builder.setSpout("spout_head", new RandomLogSpout(), paralellism*4).setNumTasks(160);
+		builder.setSpout("spout_head", new RandomLogSpout(), paralellism*4).setNumTasks(80);
 
 		for (int i = 0; i < numBolt; i++) {
 			if (i == 0) {
-				builder.setBolt("bolt_linear_" + i, new TestBolt(), paralellism*4).setNumTasks(160)
+				builder.setBolt("bolt_linear_" + i, new TestBolt(), paralellism*4).setNumTasks(80)
 						.shuffleGrouping("spout_head");
 			} else {
 				if (i == (numBolt - 1)) {
 					builder.setBolt("bolt_output_" + i, new OutBolt("sink"),
-							paralellism*4).setNumTasks(160).shuffleGrouping(
+							paralellism*4).setNumTasks(80).shuffleGrouping(
 							"bolt_linear_" + (i - 1));
 				} else {
 					builder.setBolt("bolt_linear_" + i, new TestBolt(),
-							paralellism*4).setNumTasks(160).shuffleGrouping(
+							paralellism*4).setNumTasks(80).shuffleGrouping(
 							"bolt_linear_" + (i - 1));
 				}
 			}
